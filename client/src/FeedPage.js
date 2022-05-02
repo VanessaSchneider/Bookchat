65ac4d5bdb8a5a0d49b656d354b3249903525031
Vanessa Schneider <vanessajwade@gmail.com>
2022-05-01 21:05:46 -0400

    posts a little better

--- client/src/FeedPage.js
+++ client/src/FeedPage.js
@@ -8,7 +8,7 @@ function FeedPage({posts, users}){
 
           console.log("users", users)
             let post = []
-          if (posts && posts.length!==0) {post = posts.map(post =><div> 
+          if (posts && posts.length!==0) {post = posts.map(post =><div className="post-container"> 
            {post.user.photo? <img className = "feedSize" src = {post.user.photo}></img> : null }
              {post.user.username}
              <br></br>
