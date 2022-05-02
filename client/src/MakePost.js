07816a406c7c52b2cc0e67299698e992b8365adb
Vanessa Schneider <vanessajwade@gmail.com>
2022-05-02 12:38:39 -0400

    getting showid

--- client/src/MakePost.js
+++ client/src/MakePost.js
@@ -1,98 +1,107 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
-function MakePost({handleAddPost}) {
-  const [content, setContent] = useState("");
+function MakePost ({ handleAddPost }) {
+  const [content, setContent] = useState('')
   const [makePostIsHidden, setMakePostIsHidden] = useState(false)
-  const [user, setUser] =useState("")
-  const [show, setShow] =useState("")
-  const [search, setSearch] =useState("")
-
+  const [user, setUser] = useState('')
+  const [show, setShow] = useState('')
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
-    fetch("/me").then((response) => {
+    fetch('/me').then(response => {
       if (response.ok) {
-        response.json().then((data) => setUser(data));
+        response.json().then(data => setUser(data))
       }
-    });
-  }, []);
+    })
+  }, [])
 
-
-  if (user) {console.log(user.username)}
-
-
-
-  function handlePost(event) {
-    setContent(event.target.value);
+  if (user) {
+    console.log(user.username)
   }
 
-  function handleShow(event) {
-    setSearch(event.target.value);
+  function handlePost (event) {
+    setContent(event.target.value)
   }
 
-  function handleSearch(e){
-      e.preventDefault();
-      const searchData =
-      {name: search}
-      fetch("/search", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(searchData),
-      })
-            .then((r) => r.json())
-            .then((show) => console.log(show));
-    
-      }
-
-
-
-
-
-  function handleSubmit(e) {
-    e.preventDefault();
-      const formData = {
-         content: content,
-         user_id: user.id,
-         username: user.username
-        };
-
-
-        console.log(formData)
-      fetch("/posts", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-  })
-        .then((r) => r.json())
-        .then((newPost) => handleAddPost(newPost));
-
+  function handleShow (event) {
+    setSearch(event.target.value)
   }
 
-  function handleWritePostClick() {
+  function handleSearch (e) {
+    e.preventDefault()
+    const searchData = { name: search }
+    fetch('/search', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(searchData)
+    })
+      .then(r => r.json())
+      .then(show => setShow(show))
+  }
+
+  function handleSubmit (e) {
+    e.preventDefault()
+    const formData = {
+      content: content,
+      show_id: show.id,
+      user_id: user.id,
+      username: user.username
+    }
+
+    console.log(formData)
+    fetch('/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(r => r.json())
+      .then(newPost => handleAddPost(newPost))
+  }
+
+  function handleWritePostClick () {
     setMakePostIsHidden(makePostIsHidden => !makePostIsHidden)
   }
 
   return (
     <>
-      { makePostIsHidden ? ( 
-        <div>  
-             <form onSubmit={handleSearch}>
-            <input className = "post-size" type="text"
-             placeholder="Search for a show" onChange={handleShow} value={search} />
-            <button className="button" type="submit">Submit</button>
-          </form> 
+      {makePostIsHidden ? (
+        <div>
+          <form onSubmit={handleSearch}>
+            <input
+              className='post-size'
+              type='text'
+              placeholder='Search for a show'
+              onChange={handleShow}
+              value={search}
+            />
+            <button className='button' type='submit'>
+              Submit
+            </button>
+          </form>
 
           <form onSubmit={handleSubmit}>
-            <input className = "post-size" type="text"
-             placeholder="Write your post" onChange={handlePost} value={content} />
-            <button className="button" type="submit">Submit</button>
-          </form> 
-        </div> ) : null }
-        <button className="button" onClick={handleWritePostClick}> {makePostIsHidden ? "Hide Post Field" : "Write a Post"} </button>
+            <input
+              className='post-size'
+              type='text'
+              placeholder='Write your post'
+              onChange={handlePost}
+              value={content}
+            />
+            <button className='button' type='submit'>
+              Submit
+            </button>
+          </form>
+        </div>
+      ) : null}
+      <button className='button' onClick={handleWritePostClick}>
+        {' '}
+        {makePostIsHidden ? 'Hide Post Field' : 'Write a Post'}{' '}
+      </button>
     </>
-  );
+  )
 }
-  export default MakePost;
+export default MakePost
