4ea2dc74b570910e01a229ac3f9396dd9117e1e5
Vanessa Schneider <vanessajwade@gmail.com>
2022-05-03 22:54:49 -0400

    friend request message

--- client/src/AddFriend.js
+++ client/src/AddFriend.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 function AddFriend({user}){
     const [me, setMe] =useState("")
+    const [friendRequest, setFriendRequest]= useState(null)
 
     useEffect(() => {
         fetch("/me").then((response) => {
@@ -27,16 +28,17 @@ function Add(){
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(newFriendData)
-      })
-
-
-    }
+        body: JSON.stringify(newFriendData),
+    })
+      .then((r) => r.json())
+      .then((friendRequest) => setFriendRequest(friendRequest))}
+      
 
 
 return(
 <div>
 <button onClick={Add}> Add Friend </button>
+{friendRequest ?<h2> Friend Request Sent to {user.username}</h2> : null}
 </div>
 )}
 export default AddFriend;
