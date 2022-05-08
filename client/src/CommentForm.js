520f754309f90d9910c5575b9b8e4ed9c48b18b3
Vanessa Schneider <vanessajwade@gmail.com>
2022-05-08 11:32:27 -0400

    spacing

---
+++ client/src/CommentForm.js
@@ -0,0 +1,6 @@
+function CommentForm () {
+    return <div>This is the commentsform</div>
+  }
+  
+  export default CommentForm;
+  
--- client/src/FeedPage.js
+++ client/src/FeedPage.js
@@ -2,10 +2,14 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Comments from './Comments'
 function FeedPage ({ posts, users }) {
-  const [showComments, setShowComments]=useState(false)
+  const [showComments, setShowComments] = useState(false)
+  const [commentForm, setCommentForm] = useState(false)
 
-  function ShowTheComments(){
-setShowComments((showComments)=>!showComments)
+  function ShowTheComments () {
+    setShowComments(showComments => !showComments)
+  }
+  function showCommentForm () {
+    setCommentForm(commentForm => !commentForm)
   }
 
   let post = []
@@ -15,43 +19,42 @@ setShowComments((showComments)=>!showComments)
         <div className='user-feed-container'>
           {post.user.photo ? (
             <div>
-            <img className='feedSize' src={post.user.photo}></img> </div>
+              <img className='feedSize' src={post.user.photo}></img>{' '}
+            </div>
           ) : null}
-     
-        <div className ="username">
-          <Link to={`/users/${post.user.username}`}>{post.user.username}</Link>
-        </div>
-        <div className='post-space'>
-        <Link to={`/posts/${post.id}`}>
-          <button className = "button2"
-          >{post.content}</button>
-                </Link>
+
+          <div className='username'>
+            <Link to={`/users/${post.user.username}`}>
+              {post.user.username}
+            </Link>
           </div>
-          <div className = "writepost"> 
-          <button
-          className ="button2"
-          >Comment on Post</button>
-        </div>
-        <div className='post-container'>
-          <Link to={`/shows/${post.show.name}`}>#{post.show.name}</Link>
-        </div>
-        <button className ="button2"
-        onClick={ShowTheComments}
-        >{showComments? "Hide Comments" :
-          "Show Comments"}
+          <div className='post-space'>
+            <Link to={`/posts/${post.id}`}>
+              <button className='button2'>{post.content}</button>
+            </Link>
+          </div>
+          <div className='writepost'>
+            <button onClick={showCommentForm}>Comment on Post</button>
+          </div>
+          <div className='post-container2'>
+            <Link to={`/shows/${post.show.name}`}>#{post.show.name}</Link>
+          </div>
+          <button className='button2' onClick={ShowTheComments}>
+            {showComments ? 'Hide Comments' : 'Show Comments'}
           </button>
-        {showComments ? <Comments/> : null}
+          {showComments ? <Comments /> : null}
         </div>
       </div>
     ))
   } else {
     null
   }
-console.log(showComments)
-  return <div className ='user-feed-container'>
-  <div className ='user-feed-container'>{post}</div>
-
-  </div>
+  console.log(commentForm)
+  return (
+    <div className='user-feed-container'>
+      <div className='user-feed-container'>{post}</div>
+    </div>
+  )
 }
 
 export default FeedPage
